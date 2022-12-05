a2807944779e9a51191aefc2b5c44a20ce16f578
Stait <s.tait.1@research.gla.ac.uk>
2022-12-05 16:57:46 +0000

    Update pre_fit_py.m
    changes to fetching of current working directory. Uses python based os.chdir() instead - under the assumption that a new copy of the python analysis script is created on each run of the analysis

--- pre_fit_py.m
+++ pre_fit_py.m
@@ -5,33 +5,44 @@ function pre_fit_py
 % directory and replaces the definition of the prefix variable in the
 % notebook with the path of the current directory 
 %
-% Author S.Tait 2021 
+%
+% Currently PRE_ALPHA 
+% not tested on windows systems 
+% 
+% Author S.Tait 2022
 
+currentVersion = 0.2; 
 
-! cp ~/Dropbox/Python/FitBeats/fit_ringdowns_degenerate_cmd.ipynb . 
-
-rawText =tex_import(char(fnames('fit_ringdowns*cmd.ipynb')));
-
-out = regexp(rawText,'prefix = ','Match');
-lines = find(~cellfun(@isempty,out));
-
-oldpath = regexprep(rawText(lines),{'"prefix = \\"','\\"\\n",'},'');
-
-rawText(lines) = strrep(rawText(lines),oldpath,pwd);
-
-fid2 = fopen('fit_ringdowns_degenerate_cmd.ipynb','wt');
-
-for k =1:numel(rawText)
-    
-fprintf(fid2,'%s',string(rawText{k}));
-end
-fclose(fid2);
+pyfilepath = extractBefore(which('prebeat.m'),'prebeat.m');
+copyfile(fullfile(pyfilepath,'fit_ringdowns_degenerate_cmd.ipynb'),pwd)
+% 
+% % % ! cp ~/Dropbox/Python/FitBeats/fit_ringdowns_degenerate_cmd.ipynb . 
+% 
+% rawText =tex_import(char(fnames('fit_ringdowns*cmd.ipynb')));
+% 
+% out = regexp(rawText,'prefix = ','Match');
+% lines = find(~cellfun(@isempty,out));
+% 
+% oldpath = regexprep(rawText(lines),{'"prefix = \\"','\\"\\n",'},'');
+% 
+% rawText(lines) = strrep(rawText(lines),oldpath,pwd);
+% 
+% fid2 = fopen('fit_ringdowns_degenerate_cmd.ipynb','wt');
+% 
+% for k =1:numel(rawText)
+%     
+% fprintf(fid2,'%s',string(rawText{k}));
+% end
+% fclose(fid2);
 
 cprintf('hyper','\n\nRunning Python Analysis\n\n')
 
 !papermill fit_ringdowns_degenerate_cmd.ipynb output.ipynb
+
 pause(5)
-! mv ./fit_ringdowns_degenerate_cmd.ipynb ./deleteme.ipynb
-! mv ./output.ipynb ./fit_ringdowns_degenerate_cmd.ipynb
-! rm deleteme.ipynb
+
+movefile("fit_ringdowns_degenerate_cmd.ipynb","deleteme.ipynb")
+movefile("output.ipynb","fit_ringdowns_degenerate_cmd.ipynb")
+delete("deleteme.ipynb")
+
 end  
