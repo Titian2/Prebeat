5696219699d728bcf666e042d56289bd5ece46cd
Stait <s.tait.1@research.gla.ac.uk>
2022-12-08 10:48:06 +0000

    Update prebeat.m

--- prebeat.m
+++ prebeat.m
@@ -15,7 +15,6 @@ function prebeat(talk,samplename,auto,overtemp)
 % in the current directory and subfolders for the saved files and
 % automatically resume at the crashed point.
 %
-%       HI 
 %
 %           prebeat(verbose,'samplename')
 %
